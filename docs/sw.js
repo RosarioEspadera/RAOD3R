43cb23a6feee15d07f2c91e7e0d6c961c9f8eed2
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-24 01:13:07 +0800

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "raod3r-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/favicon.png",
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve from cache first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
